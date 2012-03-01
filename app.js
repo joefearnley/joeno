8ad45164a9cb7aaa2f597be8952b471b619724c2
Joe Fearnley <joe.fearnley@gmail.com>
2012-02-29 20:39:19 -0500

    updated app.js

--- app.js
+++ app.js
@@ -39,5 +39,6 @@ app.configure('production', function(){
 app.get('/', routes.index);
 app.get('/draw/:id', routes.draw);
 
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
