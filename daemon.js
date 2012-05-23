37ceb3e7e417d7c757f4e943a8f4067cdcfd1c57
Joe Fearnley <joe.fearnley@gmail.com>
2012-05-22 22:41:02 -0400

    Added daemon file and sql scripts

--- daemon.js
+++ daemon.js
@@ -1,60 +1,20 @@
 
-var numbers = Array();
-while(numbers.length <= 19) {
-  var number = Math.floor(Math.random() * 81);
-  if(!(number in numbers)) {
-    numbers.push(number);
+var mysql = require('mysql');
+
+var client = mysql.createClient({
+  user: 'admin',
+  password: 'password',
+});
+
+var query = client.query(
+  'SELECT * FROM draw LIMIT (1)'
+  function callback(error, results, fields) {
+    if(error){
+      throw error;
+    }
   }
-}
 
-
-//var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/joeno1');
-
-var Draw = require('./models').Draw;
-
-console.log(Draw);
-var draw = new Draw({
-  spot1: numbers[1],
-  spot2: numbers[2],
-  spot3: numbers[3],
-  spot4: numbers[4],
-  spot5: numbers[5],
-  spot6: numbers[6],
-  spot7: numbers[7],
-  spot8: numbers[8],
-  spot9: numbers[9],
-  spot10: numbers[10],
-  spot11: numbers[11],
-  spot12: numbers[12],
-  spot13: numbers[13],
-  spot14: numbers[14],
-  spot15: numbers[15],
-  spot16: numbers[16],
-  spot17: numbers[17],
-  spot18: numbers[18],
-  spot19: numbers[19],
-  spot20: numbers[20]
-});
-
-// save to mongo
-draw.save(function (err) {
-
-  console.log('lkasdfjlk;ajsdlf');
-
-  if(!err) {
-    console.log('drawing saved!');
-  } else {
-    console.log('error saving draw');
-  }
-});
-
-// retrieve from mongo
-Draw.find({}).limit(1).exec(function (err, docs) {
-    //
-    // start looping...
-    //
-    console.log(docs);
-});
-
-
+  console.log(results);
+  console.log(fields);
+  client.end();
+);
---
+++ sql/create_tables.sql
@@ -0,0 +1,29 @@
+
+CREATE TABLE draw (
+  draw_id INT AUTO_INCREMENT,
+  spot1 SMALLINT,
+  spot2 SMALLINT,
+  spot3 SMALLINT,
+  spot4 SMALLINT,
+  spot5 SMALLINT,
+  spot6 SMALLINT,
+  spot7 SMALLINT,
+  spot8 SMALLINT,
+  spot9 SMALLINT,
+  spot10 SMALLINT,
+  spot11 SMALLINT,
+  spot12 SMALLINT,
+  spot13 SMALLINT,
+  spot14 SMALLINT,
+  spot15 SMALLINT,
+  spot16 SMALLINT,
+  spot17 SMALLINT,
+  spot18 SMALLINT,
+  spot19 SMALLINT,
+  spot20 SMALLINT,
+)
+
+CREATE TABLE users (
+  email VARCHAR(50),
+  date_created TIMESTAMP,
+)
