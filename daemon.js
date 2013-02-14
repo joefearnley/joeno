5cfb9e9c9f24e032fc003cff9d5b95f6352d2270
Joe Fearnley <joe.fearnley@gmail.com>
2013-02-13 23:00:09 -0500

    Initial commit starting over.

--- daemon.js
+++ daemon.js
@@ -1,20 +1,44 @@
 
-var mysql = require('mysql');
+var models = require('./models');
+var Draw = models.Draw;
 
-var client = mysql.createClient({
-  user: 'admin',
-  password: 'password',
+
+var numbers = Array();
+while(numbers.length <= 19) {
+  var number = Math.floor(Math.random() * 81);
+  if(!(number in numbers)) {
+    numbers.push(number);
+  }
+}
+
+var draw = new Draw({
+  spot1: numbers[1],
+  spot2: numbers[2],
+  spot3: numbers[3],
+  spot4: numbers[4],
+  spot5: numbers[5],
+  spot6: numbers[6],
+  spot7: numbers[7],
+  spot8: numbers[8],
+  spot9: numbers[9],
+  spot10: numbers[10],
+  spot11: numbers[11],
+  spot12: numbers[12],
+  spot13: numbers[13],
+  spot14: numbers[14],
+  spot15: numbers[15],
+  spot16: numbers[16],
+  spot17: numbers[17],
+  spot18: numbers[18],
+  spot19: numbers[19],
+  spot20: numbers[20]
 });
 
-var query = client.query(
-  'SELECT * FROM draw LIMIT (1)'
-  function callback(error, results, fields) {
-    if(error){
-      throw error;
-    }
+draw.save(function (error) {
+  if (error) {
+    console.log('we got an error');
+  } else {
+    console.log('draw saved: ' + this.id);
   }
+});
 
-  console.log(results);
-  console.log(fields);
-  client.end();
-);
