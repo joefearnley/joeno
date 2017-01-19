112e523737753022e55448df7777dc714333ebfe
Joe Fearnley <joe.fearnley@gmail.com>
2017-01-18 21:06:15 -0500

    Getting karma up and running

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,16 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'public/js/app.js',
-      'test/*.js'
+      'test/home.spec.js'
     ],
     exclude: [],
     preprocessors: {},
     reporters: ['spec'],
-    plugins: ['karma-spec-reporter'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
--- test/home.spec.js
+++ test/home.spec.js
@@ -1,13 +1,10 @@
 'use strict';
 
-const request = require('supertest');
-const expect = require('chai').expect;
-
 describe('Home Page', function() {
 
     describe('App loaded', function() {
         it('should be loaded', function() {
-            expect(app).toBeDefined();
+            expect(app).to.not.be.undefined;
         });
     });
 
