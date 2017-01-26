708e359f330a3fd6cbb6872409c0fed75a9e373e
Joe Fearnley <joe.fearnley@gmail.com>
2017-01-26 18:13:11 +0000

    Attempting to get testing set up. What a pain

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,23 +1,8 @@
-'use strict';
 
-const request = require('supertest');
-const expect = require('chai').expect;
-
-describe('GET /', function() {
-    let server;
-
-    beforeEach(function () {
-        server = require('../server');
-    });
-
-    it('loads the index page', function(done) {
-        request(server)
-            .get('/')
-            .expect(200)
-            .expect(function(res) {
-                expect(res.text).to.contain('Joeno');
-                expect(res.text).to.contain('A keno gaming created using JavaScript');
-            })
-            .end(done);
+describe('App loaded', function() {
+    beforeEach(module('joeno'));
+    
+    it('should be loaded', function() {
+        expect(app).to.not.be.undefined;
     });
 });
